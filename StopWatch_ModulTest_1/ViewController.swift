bfe002fd8bcc34a8d6fbab293eb230d6026d6b80
進藤恒 <hisashishindo@mac.com>
2016-06-17 14:14:38 +0900

    Initial Commit

---
+++ StopWatch_ModulTest_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StopWatch_ModulTest_1
+//
+//  Created by HisashiShindo on 2016/06/17.
+//  Copyright © 2016年 HisashiShindo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
